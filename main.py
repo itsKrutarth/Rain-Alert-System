d9527145e1389b7313bd806a68d996ad1c19696b
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-09-21 09:06:37 -0400

    my home, and work location and their api calls

--- main.py
+++ main.py
@@ -17,18 +17,40 @@ API call
 https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 """
 
-city = input("Please enter your city: ")
-state = input("Please enter the state: ")
-country = input("Please enter you country code: ")
+city_home = "Northbridge"
+state_home = "MA"
+country_home = "USA"
 
-weather_by_city = "https://api.openweathermap.org/data/2.5/weather?"
-parameters = {"q": f"{city},{state},{country}", "appid": api_key}
-response = requests.get(weather_by_city, params=parameters)
-data_city = response.json()
-lat = data_city["coord"]["lat"]
-lon = data_city["coord"]["lon"]
+city_work1 = "Merrimack"
+state_work1 = "NH"
+country_work1 = "USA"
+
+city_work2 = "Raynham"
+state_work2= "MA"
+country_work2 = "USA"
+
+weather_by_city_home = "https://api.openweathermap.org/data/2.5/weather?"
+parameters_home = {"q": f"{city_home},{city_home},{city_home}", "appid": api_key}
+response_home = requests.get(weather_by_city_home, params=parameters_home)
+data_home = response_home.json()
+lat_home = data_home["coord"]["lat"]
+lon_home = data_home["coord"]["lon"]
+
+weather_by_city_work2 = "https://api.openweathermap.org/data/2.5/weather?"
+parameters_work2 = {"q": f"{city_work2},{city_work2},{city_work2}", "appid": api_key}
+response_work2 = requests.get(weather_by_city_work2, params=parameters_work2)
+data_work2 = response_home.json()
+lat_work2 = data_work2["coord"]["lat"]
+lon_work2 = data_work2["coord"]["lon"]
+
+weather_by_city_work1 = "https://api.openweathermap.org/data/2.5/weather?"
+parameters_work1 = {"q": f"{city_work1},{city_work1},{city_work1}", "appid": api_key}
+response_work1 = requests.get(weather_by_city_work1, params=parameters_work1)
+data_work1 = response_work1.json()
+lat_work1 = data_work1["coord"]["lat"]
+lon_work1 = data_work1["coord"]["lon"]
 
 weather_five_days = "https://api.openweathermap.org/data/2.5/forecast?"
-parameters = {"lat": lat, "lon": lon, "appid": api_key}
+parameters = {"lat": lat_home, "lon": lon_home, "appid": api_key, "cnt": 4}
 response = requests.get(weather_five_days, params=parameters)
 print(response.json())
